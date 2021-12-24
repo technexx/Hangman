221b0f27463af8a4a1247eb9173ae7584c212767
technexx <48839830+technexx@users.noreply.github.com>
2021-12-23 17:35:37 -0800

    hangman board drawn

--- app/src/main/java/game/of/hangman/BoardCanvas.java
+++ app/src/main/java/game/of/hangman/BoardCanvas.java
@@ -26,7 +26,7 @@ public class BoardCanvas extends View {
     private void setUpPaint() {
         mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
-        mPaint.setStyle(Paint.Style.STROKE);
+//        mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(6);
     }
 
@@ -41,11 +41,38 @@ public class BoardCanvas extends View {
     @Override
     public void onDraw(Canvas canvas) {
         mCanvas = canvas;
+
+        drawLetterBoard();
+        drawHangMan();
+    }
+
+    public void drawLetterBoard() {
         int xPos = dpConv(33);
 
         for (int i=0; i<numberOfSpaces; i++) {
             mCanvas.drawLine(dpConv(xPos), 0, dpConv(xPos+30), 0, mPaint);
             dpConv(xPos+=42);
         }
+    }
+
+    public void drawHangMan() {
+        int topXStart = 125;
+        int topXEnd = 230;
+        int topY = 60;
+
+        mCanvas.drawLine(dpConv(topXStart), dpConv(topY), dpConv(topXEnd), dpConv(topY), mPaint);
+
+        mCanvas.drawLine(dpConv(topXStart), dpConv(topY), dpConv(topXStart), dpConv(topY+30), mPaint);
+
+        mCanvas.drawLine(dpConv(topXEnd-30), dpConv(topY), dpConv(topXEnd), dpConv(topY + 30), mPaint);
+
+        mCanvas.drawLine(dpConv(topXEnd), dpConv(topY), dpConv(topXEnd), dpConv(topY + 150), mPaint);
+
+        mCanvas.drawLine(dpConv(topXEnd-50), dpConv(topY+150), dpConv(topXEnd+50), dpConv(topY + 150), mPaint);
+
+        mCanvas.drawLine(dpConv(topXEnd), dpConv(topY+120), dpConv(topXEnd-30), dpConv(topY + 150), mPaint);
+
+        mCanvas.drawLine(dpConv(topXEnd), dpConv(topY+120), dpConv(topXEnd+30), dpConv(topY + 150), mPaint);
+
     }
 }
